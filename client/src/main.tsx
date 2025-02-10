9de479d7ea027bf02dea9a15b6579645ef00b767
Jon33260 <j.mallet82@laposte.net>
2025-02-10 10:42:57 +0100

    monorepo_quest_3

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,12 +7,14 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 // Import the main app component
 import App from "./App";
+import Programs from "./pages/Programs";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
 
 // import About from "./pages/About";
 // import Contact from "./pages/Contact";
+import { getAllSeries } from "./services/requests";
 
 /* ************************************************************************* */
 
@@ -24,6 +26,11 @@ const router = createBrowserRouter([
     element: <App />, // Renders the App component for the home page
   },
   // Try adding a new route! For example, "/about" with an About component
+  {
+    path: "/programs",
+    element: <Programs />,
+    loader: getAllSeries,
+  },
 ]);
 
 /* ************************************************************************* */
